63d6fa1fa260499ffc20bd6df85383e15af28137
Igor Bochkor <igorbochkor@gmail.com>
2020-12-27 20:04:18 +0200

    added some refactor

--- src/main/java/matrix/Main.java
+++ src/main/java/matrix/Main.java
@@ -1,50 +1,55 @@
 package matrix;
 
 public class Main {
-    private int first;
+    private int firstParametrCell;
     private int firstTemp;
-    private int second;
+    private int secondParametrCell;
     private int secondTemp;
 
-    public String matrixAndWord(String sizeMatrix, String word) {
-        if (sizeMatrix == null || word == null) {
+    public String matrixAndWord(String squareMatrix, String word) {
+        if (squareMatrix == null || word == null) {
             throw new NullPointerException();
         }
-        if (sizeMatrix.equals("") || word.equals("")) {
+        if (squareMatrix.equals("") || word.equals("")) {
             return "";
         }
-        if (!isSizeMatrixSquare(sizeMatrix) || !isFirstLargerThenSecond(sizeMatrix, word)
-                || !isOnlyLowerCase(sizeMatrix, word)) {
+        if (!isSizeMatrixSquare(squareMatrix) || !isFirstLargerThenSecond(squareMatrix, word)
+                || !isOnlyLowerCase(squareMatrix, word)) {
             throw new RuntimeException("Incorrect data");
         }
 
         StringBuilder builder = new StringBuilder();
-        for (int j = 0; j < sizeMatrix.length(); j++) {
-            if (word.charAt(0) == sizeMatrix.charAt(j)) {
-                first = j / (int) Math.sqrt(sizeMatrix.length());
-                second = j % (int) Math.sqrt(sizeMatrix.length());
-                builder.append("[").append(first).append(",").append(second)
-                        .append("]").append("->");
+        for (int j = 0; j < squareMatrix.length(); j++) {
+            if (word.charAt(0) == squareMatrix.charAt(j)) {
+                firstParametrCell = j / matrixSizeParametr(squareMatrix);
+                secondParametrCell = j % matrixSizeParametr(squareMatrix);
+                builder.append("[").append(firstParametrCell).append(",")
+                        .append(secondParametrCell).append("]").append("->");
                 break;
             }
         }
 
         for (int i = 1; i < word.length(); i++) {
-            for (int j = 0; j < sizeMatrix.length(); j++) {
-                if (word.charAt(i) == sizeMatrix.charAt(j)) {
-                    firstTemp = j / (int) Math.sqrt(sizeMatrix.length());
-                    secondTemp = j % (int) Math.sqrt(sizeMatrix.length());
-                    if (Math.abs(firstTemp - first) <= 1 && Math.abs(secondTemp - second) <= 1) {
-                        first = firstTemp;
-                        second = secondTemp;
-                        builder.append("[").append(first).append(",").append(second)
-                                .append("]").append("->");
+            for (int j = 0; j < squareMatrix.length(); j++) {
+                if (word.charAt(i) == squareMatrix.charAt(j)) {
+                    firstTemp = j / matrixSizeParametr(squareMatrix);
+                    secondTemp = j % matrixSizeParametr(squareMatrix);
+                    if (Math.abs(firstTemp - firstParametrCell) <= 1
+                            && Math.abs(secondTemp - secondParametrCell) <= 1) {
+                        firstParametrCell = firstTemp;
+                        secondParametrCell = secondTemp;
+                        builder.append("[").append(firstParametrCell).append(",")
+                                .append(secondParametrCell).append("]").append("->");
                         break;
                     }
                 }
             }
         }
         return builder.substring(0, builder.length() - 2);
+    }
+
+    public int matrixSizeParametr(String matrix) {
+        return (int) Math.sqrt(matrix.length());
     }
 
     public boolean isSizeMatrixSquare(String matrixLength) {
